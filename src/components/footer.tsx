720e6d693cee7152713d8ab04effd55723817c93
Andrew Martinez <martinezjandrew@gmail.com>
2025-06-20 04:09:09 -0500

    Initial footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div>
-      <p>Footer</p>
+    <div className="grid grid-cols-3 gap-4">
+      <div className=''>@ 2025</div>
+      <div className=''>created by andrew martinez</div>
+      <div className=''>source code</div>
     </div>
   )
 }
