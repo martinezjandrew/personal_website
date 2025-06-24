c617c7c8588de79b2bf2c31adf77eaadc706b0b6
Andrew Martinez <martinezjandrew@gmail.com>
2025-06-24 15:17:45 -0500

    updated footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div className="grid grid-cols-3 gap-4">
-      <div className=''>@ 2025</div>
-      <div className=''>created by andrew martinez</div>
-      <div className=''>source code</div>
+    <div className="grid grid-cols-3 gap-4 text-taupe-gray border-double border-taupe-gray border-8">
+      <div className='bg-floral-white'>@2025</div>
+      <div className='bg-floral-white'>created by andrew martinez</div>
+      <div className='bg-floral-white'>source code</div>
     </div>
   )
 }
