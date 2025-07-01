2c2724199bb54874ea9103f6415540f62908f476
Andrew Martinez <martinezjandrew@gmail.com>
2025-07-01 18:22:34 -0500

    completed projects logic and data definition

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -1,13 +1,11 @@
-import React from "react";
-import Body from "../../components/body.tsx";
-import Markdown from "react-markdown";
-import wego from "./wego.md";
+import React, { useState } from "react";
 import wego_yml from "./wego.yml";
 
 interface Project {
   title: string;
   description: string;
   stack: string[];
+  integrated_apis: string;
   demo: string;
   source: string;
 }
@@ -18,6 +16,7 @@ class ProjectClass implements Project {
   stack: string[];
   demo: string;
   source: string;
+  integrated_apis: string;
 
   constructor(
     title: string,
@@ -25,53 +24,84 @@ class ProjectClass implements Project {
     stack: string[],
     demo: string,
     source: string,
+    integrated_apis: string,
   ) {
     this.title = title;
     this.description = description;
     this.stack = stack.slice();
     this.demo = demo;
     this.source = source;
+    this.integrated_apis = integrated_apis;
   }
 
   render() {
+    const [visible, setVisibile] = useState(false);
+
+    const onClick = () => {
+      setVisibile(!visible);
+      console.log(visible);
+    };
+
     return (
-      <div>
-        <div className="text-4xl font-cascadia-code">{this.title}</div>
-        <p>{this.description}</p>
-        <ul>
-          {this.stack.map((tool) => (
-            <li>{tool}</li>
-          ))}
-        </ul>
-        <a href={this.demo}>View the demo!</a>
-        <a href={this.source}>View the source code!</a>
+      <div className="md space-y-4 min-w-full">
+        <div
+          data-visible={visible}
+          className="text-4xl font-cascadia-code cursor-pointer hover:text-5xl hover:duration-300 hover:ease-out duration-300 ease-in data-[visible=true]:text-5xl"
+          onClick={onClick}
+        >
+          {this.title}
+        </div>
+        <div
+          id={this.title}
+          data-visible={visible}
+          className="
+          border-l-4 m-4 pl-4
+          transition-[height] overflow-hidden transform h-0 duration-900 ease-in
+          data-[visible=true]:h-76 data-[visible=true]:ease-out data-[visible=true]:duration-900 data-[visible=true]:overflow-scroll"
+        >
+          <div className="text-2xl font-cascadia-code">description</div>
+          <p>{this.description}</p>
+          <div className="grid grid-cols-2">
+            <div>
+              <div className="text-2xl font-cascadia-code">stack</div>
+              <ul>
+                {this.stack.map((tool) => (
+                  <li key={tool}>{tool}</li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <div>
+                <div className="text-2xl font-cascadia-code">apis</div>
+                <div>{this.integrated_apis}</div>
+              </div>
+              <div className="text-2xl font-cascadia-code">links</div>
+              <div>
+                <a href={this.demo}>View the demo!</a>
+              </div>
+              <div>
+                <a href={this.source}>View the source code!</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* )} */}
       </div>
     );
   }
 }
 
 function Projects() {
-  const title = "Projects";
-  const body = "# Hello everyone Testing this out";
-
-  console.log(wego_yml);
-
   const object = new ProjectClass(
     wego_yml.title,
     wego_yml.description,
     wego_yml.stack,
     wego_yml.demo,
     wego_yml.sourcecode,
+    wego_yml.integrated_apis,
   );
 
-  // return <Body title={title} body={body} />;
-  // return <Markdown>{wego}</Markdown>;
-  // return (
-  //   <div className="md">
-  //     <Markdown>{wego}</Markdown>
-  //   </div>
-  // );
-  return <div>{object.render()}</div>;
+  return object.render();
 }
 
 export default Projects;
