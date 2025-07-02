1c8e510dc228bae19007fde56eba392ac8483660
Andrew Martinez <martinezjandrew@gmail.com>
2025-07-02 17:45:47 -0500

    added resume

--- src/pages/aboutme/resume.tsx
+++ src/pages/aboutme/resume.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import Body from '../../components/body.tsx';
+import React from "react";
+import Body from "../../components/body.tsx";
 
 function Resume() {
   const title = "Resume";
   const body = "Imagine there was a resume right here...";
 
   return (
-    < Body title={title} body={body} />
-  )
+    <div className="md w-full h-full">
+      <h1>Resume</h1>
+      <p>Take a gander at my resume! It's what I send to recruiters.</p>
+      <object
+        data="https://gfmgwywzylahennclcpq.supabase.co/storage/v1/object/public/upload//martinez-resume.pdf"
+        type="application/pdf"
+        width="100%"
+        height="750px"
+        className="border-10 border-double"
+      >
+        <p>Unable to display PDF file.</p>
+      </object>
+    </div>
+  );
 }
 
 export default Resume;
