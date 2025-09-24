ea03a11ac40a91ad633378d217e5fc2bf441d13e
Andrew Martinez <martinezjandrew@gmail.com>
2025-09-24 11:04:28 -0500

    Added link to github for 'source code' on footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
   return (
     <div className="bg-floral-white grid grid-cols-3 gap-4 text-taupe-gray border-double border-taupe-gray border-8">
-      <div className=''>@2025</div>
-      <div className=''>created by andrew martinez</div>
-      <div className=''>source code</div>
+      <div className="">@2025</div>
+      <div className="">created by andrew martinez</div>
+      <a
+        className="underline-offset-1"
+        href="https://github.com/martinezjandrew/personal_website"
+      >
+        <u>source code</u>
+      </a>
     </div>
-  )
+  );
 }
 
 export default Footer;
